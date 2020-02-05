a93ebbf37e9327bbd9e167a217114da03a605ad1
Vinicius Louzada de Souza <vinicius.louzadasouza@gmail.com>
2020-02-05 00:39:41 -0300

    feat: adiciona componente de seleção de idioma

---
+++ src/components/main/components/language-select.component.tsx
@@ -0,0 +1,45 @@
+import React, { RefObject } from "react";
+import styled from "styled-components";
+import { useAppContext } from "../../../store/store";
+import { CHANGE_LANGUAGE_OPT } from "../../../store/store";
+
+type LanguageSelectComponentProps = {
+  inputRef: RefObject<HTMLTextAreaElement>;
+};
+
+const LanguageSelectComponent: React.FC<LanguageSelectComponentProps> = ({
+  inputRef
+}) => {
+  const { dispatch } = useAppContext();
+  return (
+    <TranslatorSelect
+      onChange={e => {
+        if (inputRef.current) {
+          inputRef.current.value = "";
+        }
+        dispatch({
+          type: CHANGE_LANGUAGE_OPT,
+          payload: { translatorOption: e.target.value }
+        });
+      }}
+    >
+      <option value="pt-pru">Pt -> Pru</option>
+      <option value="pru-pt">Pru -> Pt</option>
+    </TranslatorSelect>
+  );
+};
+
+const TranslatorSelect = styled.select`
+  font-family: "Sigmar One", cursive;
+  width: 20%;
+  font-size: 14px;
+  margin-bottom: 20px;
+
+  @media screen and (max-width: 479px) {
+    width: 30%;
+    font-size: 14px;
+    background-color: white;
+  }
+`;
+
+export default LanguageSelectComponent;
