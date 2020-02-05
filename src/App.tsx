055c89136954a45c515652010afd393e59a39d86
Vinicius Louzada de Souza <vinicius.louzadasouza@gmail.com>
2020-02-05 00:52:00 -0300

    feat: adiciona componentes dentro do provider de contexto

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,24 @@
 import React from "react";
+import styled from "styled-components";
+import { AppContextProvider } from "./store/store";
 
-const App = () => {
-  return <div className="App"></div>;
+import CloudBackground from "./components/clouds-background/clouds-background.component";
+import Main from "./components/main/main.component";
+
+const App: React.FC = () => {
+  return (
+    <AppWrapper>
+      <CloudBackground />
+      <AppContextProvider>
+        <Main />
+      </AppContextProvider>
+    </AppWrapper>
+  );
 };
+
+const AppWrapper = styled.div`
+  width: 100%;
+  height: 100%;
+`;
 
 export default App;
